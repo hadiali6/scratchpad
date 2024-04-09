829eccd4aa07070f7ed8ee4103135a05eff86733
Hadi Ali <hadidub2006@gmail.com>
2024-04-09 07:03:59 -0700

    fix set() and add comments

--- init.lua
+++ init.lua
@@ -5,31 +5,37 @@ local capi = {
     screen = screen
 }
 
+---@class scratchpad
+---@field has_been_run boolean: If the scratchpad has been run in any way.
+---@field command string: Shell command used to spawn a client.
+---@field options table: Proporties applied to the client as scratchpad.
+---@field client client?: Current scratchpad client.
+---@field screen screen: The screen that the scratchpad displays to.
 local scratchpad = {}
-local has_been_run = false
 
+---Constructor for the scratchpad class.
+---@return table: Scratchpad object.
 function scratchpad:new(args)
     local obj = {}
-
     self.__index = self
     setmetatable(obj, self)
-
-    obj.command = args.command
-    obj.rule    = args.rule
-    obj.options = args.options
-    obj.client  = args.client
-    obj.screen  = args.screen or awful.screen.focused()
-
+    obj.has_been_run = false
+    obj.command      = args.command or "alacritty"
+    obj.options      = args.options
+    obj.client       = args.client
+    obj.screen       = args.screen or awful.screen.focused()
     return obj
 end
 
+---Getter for client options table. Will define any property if it wasn't already defined.
+---@return table: Client options.
 function scratchpad:get_client_options()
     local tbl = {}
-    tbl.ontop    = self.options.ontop    or false
-    tbl.above    = self.options.above    or false
-    tbl.hidden   = self.options.hidden   or false
-    tbl.sticky   = self.options.sticky   or false
-    tbl.floating = self.options.floating or false
+    tbl.floating     = self.options.floating     or false
+    tbl.skip_taskbar = self.options.skip_taskbar or false
+    tbl.ontop        = self.options.ontop        or false
+    tbl.above        = self.options.above        or false
+    tbl.sticky       = self.options.sticky       or false
     if self.options.geometry then
         tbl.geometry = {
             width  = self.options.geometry.width  or 1200,
@@ -48,67 +54,75 @@ function scratchpad:get_client_options()
     return tbl
 end
 
+---Apply client properties to the scratchpad as per defined in options table.
 function scratchpad:apply_props()
+    if not self.client then
+        return
+    end
     local props = self:get_client_options()
-    if self.client then
-        local screen_workarea = self.screen.workarea
-        local screen_geometry = self.screen.geometry
+    local screen_workarea = self.screen.workarea
+    local screen_geometry = self.screen.geometry
 
-        if props.floating then
-            self.client:set_floating(true)
-        end
-        if props.ontop then
-            self.client.ontop = true
-        end
-        if props.above then
-            self.client.above = true
-        end
-        if props.sticky then
-            self.client.sticky = true
-        end
-        if props.width and props.width <= 1 then
-            props.width = screen_workarea.width * props.width
-        end
-        if props.height and props.height <= 1 then
-            props.height = screen_workarea.height * props.height
-        end
-        awful.client.property.set(self.client, "floating_geometry", self.client:geometry({
-            x      = screen_geometry.x + props.geometry.x,
-            y      = screen_geometry.y + props.geometry.y,
-            width  = props.geometry.width,
-            height = props.geometry.height,
-        }))
-
+    if props.floating then
+        self.client:set_floating(true)
+    end
+    if props.skip_taskbar then
         self.client.skip_taskbar = true
     end
+    if props.ontop then
+        self.client.ontop = true
+    end
+    if props.above then
+        self.client.above = true
+    end
+    if props.sticky then
+        self.client.sticky = true
+    end
+    if props.width and props.width <= 1 then
+        props.width = screen_workarea.width * props.width
+    end
+    if props.height and props.height <= 1 then
+        props.height = screen_workarea.height * props.height
+    end
+    awful.client.property.set(self.client, "floating_geometry", self.client:geometry({
+        x      = screen_geometry.x + props.geometry.x,
+        y      = screen_geometry.y + props.geometry.y,
+        width  = props.geometry.width,
+        height = props.geometry.height,
+    }))
 end
 
+---Disable any client properties applied to the scratchpad as per defined in options table.
 function scratchpad:unapply_props()
+    if not self.client then
+        return
+    end
     local props = self:get_client_options()
-    if self.client then
-        if self.client.hidden then
-            self.client.hidden = false
-            self.client:move_to_tag(awful.tag.selected(self.screen))
-        end
-        if props.floating then
-            self.client:set_floating(false)
-        end
-        if props.ontop then
-            self.client.ontop = false
-        end
-        if props.above then
-            self.client.above = false
-        end
-        if props.sticky then
-            self.client.sticky = false
-        end
+    if self.client.hidden then
+        self.client.hidden = false
+        self.client:move_to_tag(awful.tag.selected(self.screen))
+    end
+    if props.floating then
+        self.client:set_floating(false)
+    end
+    if props.skip_taskbar then
         self.client.skip_taskbar = false
+    end
+    if props.ontop then
+        self.client.ontop = false
+    end
+    if props.above then
+        self.client.above = false
+    end
+    if props.sticky then
+        self.client.sticky = false
     end
 end
 
-function scratchpad:startup()
-    if has_been_run == false then
-        has_been_run = true
+---Connect unmanage signal if there hasn't been usage of toggle() or set()
+function scratchpad:connect_unmanage_signal()
+    if self.has_been_run == false then
+        self.has_been_run = true
         capi.client.connect_signal("request::unmanage", function (current_client)
             if self.client == current_client then
                 self.client = nil
@@ -117,54 +131,59 @@ function scratchpad:startup()
     end
 end
 
+---Enable scratchpad visiblity.
+function scratchpad:turn_on()
+    self.client.hidden = false
+    self.client:move_to_tag(awful.tag.selected(self.screen))
+    self.client:raise()
+    capi.client.focus = self.client
+end
+
+---Disable scratchpad visiblity.
+function scratchpad:turn_off()
+    self.client.hidden = true
+    local client_tags = self.client:tags()
+    for i, _ in pairs(client_tags) do
+        client_tags[i] = nil
+    end
+    self.client:tags(client_tags)
+end
+
+---Toggle current scratchpad clients visiblity. If there isnt one, spawn a new one.
 function scratchpad:toggle()
-    self:startup()
+    self:connect_unmanage_signal()
     if not self.client then
-        local initial_apply; initial_apply = function (c)
-            self.client = c
+        local initial_apply
+        initial_apply = function (client)
+            self.client = client
             self:apply_props()
             capi.client.disconnect_signal("request::manage", initial_apply)
         end
-
         capi.client.connect_signal("request::manage", initial_apply)
         awful.spawn(self.command, false)
     else
-        if self.client:isvisible() == false then
-            self.client.hidden = true
-            self.client:move_to_tag(awful.tag.selected(self.screen))
-        end
-
         if self.client.hidden then
-            self.client.hidden = false
-            self.client:raise()
-            capi.client.focus = self.client
+            self:turn_on()
         else
-            self.client.hidden = true
-            local ctags = self.client:tags()
-            for i, _ in pairs(ctags) do
-                ctags[i] = nil
-            end
-            self.client:tags(ctags)
+            self:turn_off()
         end
     end
 end
 
+---Set a client to be the scratchpad. If it is already a scratchpad, disable scratchpad status.
+---@param client client: Client to get set to the current scratchpad.
 function scratchpad:set(client)
-    local function setscratch(c)
-        self.client = c
-        self:apply_props()
-    end
-    self:startup()
+    self:connect_unmanage_signal()
     if not self.client then
         self.client = client
-        setscratch(self.client)
+        self:apply_props()
     else
         self:unapply_props()
         if self.client == client then
             self.client = nil
         else
             self.client = client
-            setscratch(client)
+            self:apply_props()
         end
     end
 end
