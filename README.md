7f492fbc32ea0ece35561d3f8b12e790081970ec
Hadi Ali <hadidub2006@gmail.com>
2024-05-07 05:44:29 -0700

    add-feature: groups, one-only, more helper functions. fix: cleanup. docs: keep up-to-date.

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ local scratchpad = require("scratchpad")
 local pads = {}
 pads.first_pad = scratchpad:new({ -- Initialize scratchpad object.
     command = "alacritty",        -- Command run if there isnt already a client set.
+    group   = pads,
     client_options = {            -- All options are optional.
         floating     = true,
         ontop        = false,
@@ -41,10 +42,12 @@ pads.first_pad = scratchpad:new({ -- Initialize scratchpad object.
     },
     scratchpad_options = { -- These are the defaults if you don't specify scratchpad_options.
         reapply_options = false,
+        only_one        = false,
     },
 })
 pads.second_pad = scratchpad:new({
     command = "qalculate-qt",
+    group   = pads,
     client_options = {
         floating     = true,
         ontop        = false,
@@ -60,6 +63,7 @@ pads.second_pad = scratchpad:new({
     },
     scratchpad_options = {
         reapply_options = false,
+        only_one        = false,
     },
 })
 ```
@@ -82,4 +86,27 @@ awful.key({ modkey, "Alt" }, "F2", function(c)
 end),
 ```
 ### Details
-See the [wiki](WIKI.md)
+| Scratchpad Fields    | Type       | Description                                     |
+| :------------------- | :--------: | :---------------------------------------------- |
+| `command`            | `string?`  | Shell command used to spawn a client.           |
+| `group`              | `table?`   | A common group of scratchpads.                  |
+| `client`             | `client?`  | Current scratchpad client.                      |
+| `screen`             | `screen`   | The screen that the scratchpad displays to.     |
+| `client_options`     | `table`    | Proporties applied to the client as scratchpad. |
+| `scratchpad_options` | `table`    | Additional features added to the scratchpad.    |
+
+| Scratchpad Functions                      | Return                                      | Parameters                                                                                   | Description                                                                                                                                                |
+| :---------------------------------------- | :------------------------------------------ | :------------------------------------------------------------------------------------------- | :--------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `object:new(args)`                        | `table`: Scratchpad object.                 | `table`: Arguments.                                                                          | Constructor for the scratchpad class.                                                                                                                      |
+| `object:get_scratchpad_options()`         | `table`: Options for the scratchpad.        | n/a                                                                                          | Gets scratchpad options table and defines any property if it wasn't already defined.                                                                       |
+| `object:get_client_options()`             | `table`: Options for the scratchpad client. | n/a                                                                                          | Gets client options table and defines any property if it wasn't already defined.                                                                           |
+| `object:set_all_boolean_client_options`   | `void`                                      | `boolean`: What to set to all client options. `table?`: Table of options to iterate through. | Sets all of the boolean values of the client options to a given boolean.                                                                                   |
+| `object:enable_client_options()`          | `void`                                      | n/a                                                                                          | Enable client properties to the scratchpad as per defined in options table.                                                                                |
+| `object:disable_client_options()`         | `void`                                      | n/a                                                                                          | Disable any client properties applied to the scratchpad as per defined in the client_options table.                                                        |
+| `object:turn_off_other_scratchpads()`     | `void`                                      | n/a                                                                                          | Turns off any scratchpad within the same group that is currently visible. Note: requires a group table.                                                    |
+| `object:apply_client_to_scratchpad()`     | `void`                                      | n/a                                                                                          | Applies a client from the request::manage client signal to the scratchpad. Used for when there isnt a current client within the scratchpad.                |
+| `object:turn_on()`                        | `void`                                      | n/a                                                                                          | Enable current scratchpad client visibility.                                                                                                               |
+| `object:turn_off()`                       | `void`                                      | n/a                                                                                          | Disable current scratchpad client visibility.                                                                                                              |
+| `object:toggle_visibility()`              | `void`                                      | n/a                                                                                          | Toggle current scratchpad client visibility. If there isnt one, spawn a new one.                                                                           |
+| `object:set_client_to_scratchpad(client)` | `void`                                      | `client`: Client to get set to the current scratchpad.                                       | Set client as scratchpad. Do not use, instead use `object:toggle_scratched_status()` for a "set client as scratchpad" functionality.                       |
+| `object:toggle_scratched_status(client)`  | `void`                                      | `client`: Client to get set to the current scratchpad.                                       | Toggle whether or not the focused client is the scratchpad. If it is already a scratchpad, disable its scratchpad status. Otherwise set as the scratchpad. |
--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ local capi = {
 }
 
 ---@class scratchpad
----@field has_been_run boolean: If the scratchpad has been run in any way.
 ---@field command string?: Shell command used to spawn a client.
+---@field group table?: A common group of scratchpads.
 ---@field client client?: Current scratchpad client.
 ---@field screen screen: The screen that the scratchpad displays to.
 ---@field client_options table: Proporties applied to the client as scratchpad.
@@ -31,16 +31,23 @@ function scratchpad:new(args)
     }
     local default_scratchpad_options = {
         reapply_options = false,
+        only_one        = false,
     }
     local object = {}
     self.__index = self
     setmetatable(object, self)
     object.has_been_run       = false
     object.command            = args.command
+    object.group              = args.group
     object.client             = args.client
     object.screen             = args.screen             or awful.screen.focused()
     object.client_options     = args.client_options     or default_client_options
     object.scratchpad_options = args.scratchpad_options or default_scratchpad_options
+    capi.client.connect_signal("request::unmanage", function(current_client)
+        if self.client == current_client then
+            self.client = nil
+        end
+    end)
     return object
 end
 
@@ -49,6 +56,7 @@ end
 function scratchpad:get_scratchpad_options()
     local options = {}
     options.reapply_options = self.scratchpad_options.reapply_options or false
+    options.only_one        = self.scratchpad_options.only_one        or false
     return options
 end
 
@@ -79,83 +87,79 @@ function scratchpad:get_client_options()
     return options
 end
 
+---@param bool boolean: What to set to all client options.
+---@param client_options table?: Table of options to iterate through.
+function scratchpad:set_all_boolean_client_options(bool, client_options)
+    client_options = client_options or self:get_client_options()
+    for option, _ in pairs(client_options) do
+        if
+            client_options[option]
+            and type(client_options[option]) == "boolean"
+        then
+            self.client[option] = bool
+        end
+    end
+end
+
 ---Enable client properties to the scratchpad as per defined in options table.
-function scratchpad:enable_properties()
-    if not self.client then
-        return
-    end
+function scratchpad:enable_client_options()
+    if not self.client then return end
     local client_options = self:get_client_options()
-    local screen_geometry = self.screen.geometry
-    if client_options.floating then
-        self.client:set_floating(true)
-    end
-    if client_options.skip_taskbar then
-        self.client.skip_taskbar = true
-    end
-    if client_options.ontop then
-        self.client.ontop = true
-    end
-    if client_options.above then
-        self.client.above = true
-    end
-    if client_options.sticky then
-        self.client.sticky = true
-    end
     awful.client.property.set(
         self.client,
         "floating_geometry",
         self.client:geometry({
-            x      = screen_geometry.x + client_options.geometry.x,
-            y      = screen_geometry.y + client_options.geometry.y,
+            x      = self.screen.geometry.x + client_options.geometry.x,
+            y      = self.screen.geometry.y + client_options.geometry.y,
             width  = client_options.geometry.width,
             height = client_options.geometry.height,
         })
     )
+    self:set_all_boolean_client_options(true, client_options)
 end
 
 ---Disable any client properties applied to the scratchpad as per defined in the client_options table.
-function scratchpad:disable_properties()
-    if not self.client then
-        return
-    end
+function scratchpad:disable_client_options()
+    if not self.client then return end
     local client_options = self:get_client_options()
     if self.client.hidden then
         self.client.hidden = false
         self.client:move_to_tag(awful.tag.selected(self.screen))
     end
-    if client_options.floating then
-        self.client:set_floating(false)
-    end
-    if client_options.skip_taskbar then
-        self.client.skip_taskbar = false
-    end
-    if client_options.ontop then
-        self.client.ontop = false
-    end
-    if client_options.above then
-        self.client.above = false
-    end
-    if client_options.sticky then
-        self.client.sticky = false
+    self:set_all_boolean_client_options(false, client_options)
+end
+
+function scratchpad:turn_off_other_scratchpads()
+    for _, scratchpad_object in pairs(self.group) do
+        if
+            scratchpad_object.client
+            and self.client ~= scratchpad_object.client
+            and scratchpad_object.client.hidden == false
+        then
+            scratchpad_object:turn_off()
+        end
     end
 end
 
----Connect unmanage signal if there hasn't been usage of toggle_visibility() or toggle_scratched_status()
-function scratchpad:connect_unmanage_signal()
-    if self.has_been_run == false then
-        self.has_been_run = true
-        capi.client.connect_signal("request::unmanage", function(current_client)
-            if self.client == current_client then
-                self.client = nil
-            end
-        end)
+---Applies a client from the request::manage client signal to the scratchpad.
+---Used for when there isnt a current client within the scratchpad.
+function scratchpad:apply_client_to_scratchpad()
+    local application
+    application = function(client)
+        self.client = client
+        self:enable_client_options()
+        capi.client.disconnect_signal("request::manage", application)
     end
+    capi.client.connect_signal("request::manage", application)
 end
 
 ---Enable current scratchpad client visibility.
 function scratchpad:turn_on()
+    if self.scratchpad_options.only_one then
+        self:turn_off_other_scratchpads()
+    end
     if self.scratchpad_options.reapply_options then
-        self:enable_properties()
+        self:enable_client_options()
     end
     self.client.hidden = false
     self.client:move_to_tag(awful.tag.selected(self.screen))
@@ -175,7 +179,6 @@ end
 
 ---Toggle current scratchpad client visibility. If there isnt one, spawn a new one.
 function scratchpad:toggle_visibility()
-    self:connect_unmanage_signal()
     if self.client then
         if self.client.hidden then
             self:turn_on()
@@ -183,22 +186,19 @@ function scratchpad:toggle_visibility()
             self:turn_off()
         end
     else
-        local initial_apply
-        initial_apply = function(client)
-            self.client = client
-            self:enable_properties()
-            capi.client.disconnect_signal("request::manage", initial_apply)
-        end
-        capi.client.connect_signal("request::manage", initial_apply)
+        self:apply_client_to_scratchpad()
         if self.command then
             awful.spawn(self.command, false)
+            if self.scratchpad_options.only_one then
+                self:turn_off_other_scratchpads()
+            end
         end
     end
 end
 
 function scratchpad:set_client_to_scratchpad(client)
     self.client = client
-    self:enable_properties()
+    self:enable_client_options()
     self.client:raise()
 end
 
@@ -206,9 +206,8 @@ end
 ---If it is already a scratchpad, disable its scratchpad status. Otherwise set as the scratchpad.
 ---@param client client: Client to get set to the current scratchpad.
 function scratchpad:toggle_scratched_status(client)
-    self:connect_unmanage_signal()
     if self.client then
-        self:disable_properties()
+        self:disable_client_options()
         if self.client == client then
             self.client = nil
         else
